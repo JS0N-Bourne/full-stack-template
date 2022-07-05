75f8fb19921c43da1e61a1403d3a399cb0bac1a1
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-07-04 19:03:36 -0500

    Added variables and Mongo connecion

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let dbConnectionString = process.env.DB_STRING,
+  dbName = 'practicedatabase',
+  collection
+
+MongoClient.connect(dbConnectionString)
+  .then(client => {
+    console.log(`Connected to Database`)
+    db = client.db(dbName)
+    collection = db.collection('collectiondatabase')
+  })
